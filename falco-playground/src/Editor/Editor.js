670b80079f46fd80fcb0d8a1932a2acdc07271ce
Jason Dellaluce <jasondellaluce@gmail.com>
2022-11-16 11:57:09 +0000

    new: add falco language
    Signed-off-by: Jason Dellaluce <jasondellaluce@gmail.com>

--- falco-playground/src/Editor/Editor.js
+++ falco-playground/src/Editor/Editor.js
@@ -2,21 +2,11 @@ import React from 'react';
 import CodeMirror from '@uiw/react-codemirror';
 import { StreamLanguage } from "@codemirror/language"
 import { yaml } from "@codemirror/legacy-modes/mode/yaml"
+import { Falco, FalcoLanguageHighlight } from "./FalcoLanguage"
+import { syntaxHighlighting } from "@codemirror/language"
 import './Editor.css';
 
-
-const initialContnet = `#################################
-# Start writing your Falco rules!
-#################################
-
-# Add new rules, like this one
-- rule: The program "sudo" is run in a container
-  desc: An event will trigger every time you run sudo in a container
-  condition: evt.type = execve and evt.dir=< and container.id != host and proc.name = sudo
-  output: "Sudo run in container (user=%user.name %container.info parent=%proc.pname cmdline=%proc.cmdline)"
-  priority: ERROR
-  tags: [users, container]
-`;
+const initialContnet = `fd.name contains "pippo" and fd.name = 5`;
 
 function Editor() {
   const onChange = React.useCallback((value, viewUpdate) => {
@@ -25,7 +15,7 @@ function Editor() {
   return (
     <CodeMirror
       value={initialContnet}
-      extensions={[StreamLanguage.define(yaml)]}
+      extensions={[Falco(), syntaxHighlighting(FalcoLanguageHighlight)]}
       onChange={onChange}
       minHeight={"600px"}
     />
