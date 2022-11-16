8076e0a3fdd0c4ac0f75a4023fb08793fad3c19a
Giovanni Baleani <giovanni.baleani@sysdig.com>
2022-11-16 22:45:01 +0100

    handle 2 or more warnings

--- falco-playground/src/Editor/Linter/index.js
+++ falco-playground/src/Editor/Linter/index.js
@@ -5,41 +5,55 @@ const falcoLinter = linter(view => {
   let diagnostics = [];
   
   // ... an offset from falco rule validation json response
-  const offset = 98;
-
-  // the end of selection
-  let end = 0;
+  const falcoWarnings = [
+    {
+        message: "An unknown top-level object is in the rules content. It will be ignored.",
+        offset: 98,
+        end: 0,
+    },
+    {
+        message: "Falco rule syntax is wrong (fake)",
+        offset: 149,
+        end: 0,
+    }
+  ];
 
   syntaxTree(view.state).cursor().iterate(node => {
     try {
         let text = view.viewState.state.doc.text.join(' ');
         text = text.substring(node.from, node.to);
         
-        if (node.from == offset) {
-            end = node.to;
-        }
+        falcoWarnings.forEach((item) => {
+            if (item.offset == node.from) {
+                item.end = node.to;
+            }
+        });
 
         console.log("falco linter: ",  node.name, 
             node.index, node.from, node.to, 
-            " - ", offset, end,
-            text);
+            text, falcoWarnings);
         
     } catch (e) {
         console.log('trust me: ignore this', e);
     }
   });
   // simulation of a problem
-  diagnostics.push({
-    from: offset,
-    to: end,
-    severity: "warning",
-    // severity: "error",
-    source: "Falco® Engine (WASM)",
-    message: "Falco rule syntax is wrong",
-    // actions: [{
-    //     name: "Remove",
-    //     apply(view, from, to) { view.dispatch({changes: {from, to}}) }
-    // }]
+
+  falcoWarnings.forEach((item) => {
+    if (item.offset < item.end) {
+        diagnostics.push({
+            from: item.offset,
+            to: item.end,
+            severity: "warning",
+            // severity: "error",
+            source: "Falco® Engine (WASM)",
+            message: item.message,
+            // actions: [{
+            //     name: "Remove",
+            //     apply(view, from, to) { view.dispatch({changes: {from, to}}) }
+            // }]
+        });
+    }
   });
   return diagnostics;
 })
