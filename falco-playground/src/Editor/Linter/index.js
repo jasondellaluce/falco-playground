13a23a2200a0e52bcd8bb5bc3aa07609989b6020
Giovanni Baleani <giovanni.baleani@sysdig.com>
2022-11-16 23:31:06 +0100

    use of standard falco errors/warnings json format

--- falco-playground/src/Editor/Linter/index.js
+++ falco-playground/src/Editor/Linter/index.js
@@ -1,61 +1,81 @@
-import {syntaxTree} from "@codemirror/language"
-import {linter, Diagnostic} from "@codemirror/lint"
+import { syntaxTree } from "@codemirror/language"
+import { linter } from "@codemirror/lint"
+import falcoResponse from "./falcoResponse";
 
 const falcoLinter = linter(view => {
-  let diagnostics = [];
-  
-  // ... an offset from falco rule validation json response
-  const falcoWarnings = [
-    {
-        message: "An unknown top-level object is in the rules content. It will be ignored.",
-        offset: 98,
-        end: 0,
-    },
-    {
-        message: "Falco rule syntax is wrong (fake)",
-        offset: 149,
-        end: 0,
-    }
-  ];
+    let diagnostics = [];
 
-  syntaxTree(view.state).cursor().iterate(node => {
-    try {
-        let text = view.viewState.state.doc.text.join(' ');
-        text = text.substring(node.from, node.to);
-        
-        falcoWarnings.forEach((item) => {
-            if (item.offset == node.from) {
-                item.end = node.to;
-            }
-        });
+    let errors = falcoResponse.errors;
+    let warnings = falcoResponse.warnings;
 
-        console.log("falco linter: ",  node.name, 
-            node.index, node.from, node.to, 
-            text, falcoWarnings);
-        
-    } catch (e) {
-        console.log('trust me: ignore this', e);
-    }
-  });
-  // simulation of a problem
+    syntaxTree(view.state).cursor().iterate(node => {
+        try {
+            let text = view.viewState.state.doc.text.join(' ');
+            text = text.substring(node.from, node.to);
 
-  falcoWarnings.forEach((item) => {
-    if (item.offset < item.end) {
+            errors.forEach((item) => {
+                let locations = item.context.locations;
+                let loc = locations[locations.length-1];
+                if (loc.position.offset == node.from) {
+                    loc.position.end = node.to;
+                }
+            });
+
+            warnings.forEach((item) => {
+                let locations = item.context.locations;
+                let loc = locations[locations.length-1];
+                if (loc.position.offset == node.from) {
+                    loc.position.end = node.to;
+                }
+            });
+
+            console.log("falco linter: ", node.name,
+                node.index, node.from, node.to, text);
+
+        } catch (e) {
+            console.log('trust me: ignore this', e);
+        }
+    });
+    // simulation of a problem
+
+    warnings.forEach((item) => {
+        let locations = item.context.locations;
+        let loc = locations[locations.length-1];
+        if (loc.position.offset < loc.position.end) {
+            diagnostics.push({
+                from: loc.position.offset,
+                to: loc.position.end,
+                severity: "warning",
+                source: "Falco® Engine (WASM)",
+                message: item.codedesc,
+                // actions: [{
+                //     name: "Remove",
+                //     apply(view, from, to) { view.dispatch({changes: {from, to}}) }
+                // }]
+            });
+        }
+    });
+    errors.forEach((item) => {
+        let locations = item.context.locations;
+        let loc = locations[locations.length-1];
+        let from = loc.position.offset;
+        let to = from+1;
+        if (loc.position.offset < loc.position.end) {
+            to = loc.position.end; 
+        }
         diagnostics.push({
-            from: item.offset,
-            to: item.end,
-            severity: "warning",
-            // severity: "error",
+            from: from,
+            to: to,
+            severity: "error",
             source: "Falco® Engine (WASM)",
-            message: item.message,
+            message: item.codedesc,
             // actions: [{
             //     name: "Remove",
             //     apply(view, from, to) { view.dispatch({changes: {from, to}}) }
             // }]
         });
-    }
-  });
-  return diagnostics;
+    });
+    return diagnostics;
 })
 
 export default falcoLinter;
