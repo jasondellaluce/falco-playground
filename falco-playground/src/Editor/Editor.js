3bbee871d4d93ca42159f2b54cf0ee22d57da20d
Jason Dellaluce <jasondellaluce@gmail.com>
2022-11-16 15:55:04 +0000

    update: support mixed langs
    Signed-off-by: Jason Dellaluce <jasondellaluce@gmail.com>

--- falco-playground/src/Editor/Editor.js
+++ falco-playground/src/Editor/Editor.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import CodeMirror from '@uiw/react-codemirror';
+import { tags, styleTags } from '@lezer/highlight';
+import { HighlightStyle, syntaxHighlighting } from '@codemirror/language';
 import { Mixed } from "./MixedLanguage"
 import './Editor.css';
 
-const initialContnet = `fd.name contains "pippo" and fd.name = 5`;
+const initialContnet = `####################
+# Your custom rules!
+####################
+
+# Add new rules, like this one
+ - rule: The program "sudo" is run in a container
+   desc: An event will trigger every time you run sudo in a container
+   condition: evt.type = execve and evt.dir=< and container.id != host and proc.name = sudo
+   output: "Sudo run in container (user=%user.name %container.info parent=%proc.pname cmdline=%proc.cmdline)"
+   priority: ERROR
+   tags: [users, container]
+`;
+
+const FalcoLanguageHighlight = HighlightStyle.define([
+  { tag: tags.variableName, color: "#fc6" },
+  { tag: tags.operator, color: "#f5d" }
+]);
+
 
 function Editor() {
   const onChange = React.useCallback((value, viewUpdate) => {
@@ -12,7 +31,7 @@ function Editor() {
   return (
     <CodeMirror
       value={initialContnet}
-      extensions={[Mixed()]}
+      extensions={[Mixed(), syntaxHighlighting(FalcoLanguageHighlight)]}
       onChange={onChange}
       minHeight={"600px"}
     />
--- falco-playground/src/Editor/MixedLanguage/index.js
+++ falco-playground/src/Editor/MixedLanguage/index.js
@@ -2,14 +2,14 @@ import { parseMixed } from "@lezer/common"
 import { LRLanguage, LanguageSupport} from "@codemirror/language"
 import { Falco } from "../FalcoLanguage"
 import { YAMLLanguage } from "../YAMLLanguage"
-import { syntaxHighlighting } from "@codemirror/language"
 
 
 const mixedFalcoYAMLParser = YAMLLanguage.parser.configure({
-  wrap: parseMixed(node => {
-    console.log(node);
-    console.log(node.matchContext("condition: "));
-    console.log(node.name);
+  wrap: parseMixed((node) => {
+    if (node.name == "ConditionValue") {
+      let p = { parser: Falco().language.parser };
+      return p;
+    }
     return null;
   })
 })
